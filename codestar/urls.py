3d44422e99eba33bac1cb8b2d1b9cb4ea197cc2f
tamirgen <tamirgenkin@gmail.com>
2022-02-17 09:36:03 +0000

    deployment commit

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path("", include("blog.urls"), name="blog-urls"),
+    path('', include('blog.urls'), name='blog-urls'),
+    path('accounts/', include('allauth.urls')),
+    
 ]
